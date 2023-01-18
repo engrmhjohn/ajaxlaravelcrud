fb4c8cde37cd79363f1e3e734fc18488054c737d
Mehedi <johnsubcse@gmail.com>
2023-01-18 17:21:36 +0600

    Laravel 9 Ajax Crud with Pagination & Search Completed

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Ajax Crud</title>
+    <title>Laravel 9 Ajax Crud with Pagination & Search</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
     <link rel="stylesheet" href="http://cdn.bootcss.com/toastr.js/latest/css/toastr.min.css">    
@@ -15,8 +15,8 @@
             <div class="col-lg-2">
 
             </div>
-            <div class="col-lg-8">
-                <h2 class="text-center text-bold">Laravel 9 ajax CRUD </h2>
+            <div class="col-lg-8 mt-5">
+                <h2 class="text-center text-bold bg-secondary text-white">Laravel 9 Ajax Crud with Pagination & Search</h2>
                 <div class="row">
                     <div class="col-lg-3">
                         <a class="btn btn-success mb-2" href="" data-bs-toggle="modal" data-bs-target="#exampleModal">Add Product</a>
